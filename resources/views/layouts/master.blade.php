fab42566262f1df2f62c0e74a803981d4165265c
Mahmud <kikisalto21@gmail.com>
2021-03-10 09:56:38 +0700

    Commit Kedelepan

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,17 +8,21 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Masjid Al Iman</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="/penduduk">Penduduk</a>
+                        <a class="nav-link" href="/penduduk">Penduduk</a>
                     </li>
+                    <form class="d-flex">
+                        <input class="form-control me-2 float-end" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success float-end" type="submit">Search</button>
+                    </form>
             </div>
         </nav>
         <div class="container">
